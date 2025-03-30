327e31ef9ed50c8e8ca2ac3edb095b17b7d8467d
Edgard Alvarado <Edgard-0211@hotmail.com>
2025-03-30 10:43:06 -0500

    adding db project

---
+++ Laboratory.DB.Goals/Lab/Lists.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [lab].[Lists]
+(
+	[Id] INT NOT NULL PRIMARY KEY Identity(1,1),
+	[Name] Varchar(150) not null
+)
---
+++ Laboratory.DB.Goals/Lab/Lists_Items.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [lab].[Lists_Items]
+(
+	[Id] INT NOT NULL PRIMARY KEY Identity(1,1),
+	[ListId] INT NOT NULL,
+	[Name] Varchar(200) not null,
+	[Description] varchar(500) null,
+	[ImageUrl] varchar(250) null,
+	[Complete] bit not null Default(0)
+)
